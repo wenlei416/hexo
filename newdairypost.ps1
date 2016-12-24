ff3b5747b258ede1278fb773c0a5b43703c795de
wenlei416 <leiwen.cn@gmail.com>
2016-12-24 13:58:54 +0800

    add new posts

--- newdairypost.ps1
+++ newdairypost.ps1
@@ -1,5 +1,10 @@
-$tdiary=$tdiary=[System.IO.Directory]::GetFiles("d:\hexo\source\_posts","t???-diary.md").count+1
-$newdiaryname="t0"+ $tdiary+"-diary"
+$cancelTimes=4+1;
+# 2016-12-12~16都没写
+# 2016-12-19~20合并在一起了
+$tdiary=[System.IO.Directory]::GetFiles("d:\hexo\source\_posts","t???-diary.md").count+1+$cancelTimes
+
+$newdiarycount=$tdiary
+$newdiaryname="t0"+ $newdiarycount+"-diary"
 hexo n diary $newdiaryname
 $fullpathnewdiaryname="D:\hexo\source\_posts\"+ $newdiaryname +".md"
 $lastdiary="D:\hexo\source\_posts\t0"+ ($tdiary-1)+"-diary.md"
